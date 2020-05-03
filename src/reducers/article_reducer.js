7af06bcc4bda7d78ddde929a913e4da1c1ce26d3
jjluis <jjluis95@gmail.com>
2020-05-03 16:34:55 +0530

    Delete article_reducer.js

--- src/reducers/article_reducer.js
+++
@@ -1,8 +0,0 @@
-  export default function(state={}, action){
-    switch(action.type){
-        case 'GET_LATEST_NEWS':
-            return{...state, latest:action.payload}
-        default:
-            return state;
-    }
-}
